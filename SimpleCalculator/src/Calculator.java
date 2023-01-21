55c55a7284ccdaca03485f1efd327713eee61163
Bernize Eugene <bernize.eugene123@gmail.com>
2023-01-21 00:18:18 -0400

    Create a constructor for the class, create instance for calculator class, make declarations for jframe, textfield,jbuttons, panel and font.

--- SimpleCalculator/src/Calculator.java
+++ SimpleCalculator/src/Calculator.java
@@ -3,11 +3,39 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Calculator {
+public class Calculator implements ActionListener {
+	
+	//Declarations
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[8];
+	JButton addButton, subButton, mulButton, divButton;
+	JButton decButton, equButton, delButton, clrButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Ink Free", Font.BOLD,30);
+	
+	
+	
+	// Create constructor for the class
+	Calculator(){
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Create instance for calculator
+		Calculator calc = new Calculator();
+		
 
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
